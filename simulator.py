de4642ab03f23cbf70d2fc40765bbb4dbcf84854
Eric Callaway <ericcallaway5@gmail.com>
2022-02-24 09:33:38 -0600

    [ADD] Processes are now sorted based on arrival time.

--- simulator.py
+++ simulator.py
@@ -9,17 +9,17 @@ import os
 
 # Process Class
 class Process:
-    def __init__(self, p_id, arrive, priority, cpu_time):
+    def __init__(self, p_id, arrive, priority, cpu_time, state):
         self.p_id = p_id
         self.arrive = arrive
         self.priority = priority
-        self. cpu_time = cpu_time
+        self.cpu_time = cpu_time
+        self.state = state
     
     
 
 # Reads the file and enumerates the lines. 
 def read_file(file_path):
-    rec = 2
     list_of_processes = []
     try:
         with open(file_path, "r") as data:
@@ -27,12 +27,42 @@ def read_file(file_path):
                 if index == 0:
                     num_processes = line
                 else:
-                    process = Process(index, line[:1], line[2:3], line[4:])
+                    process = Process(index, line[:1], line[2:3], line[4:], 0)
                     list_of_processes.append(process)
                 print("Line {}: {}". format(index, line.strip()))
     except FileNotFoundError:
-        rec = None
-    return rec, num_processes, list_of_processes
+        list_of_processes = None
+    return num_processes, list_of_processes
+
+def pre_priority(self):
+    pass
+
+def round_robin(self):
+    pass
+
+
+
+
+def scheduling_process(num_processes, list_of_processes):
+    start_time = []
+    exit_time =[]
+    s_time = 0
+    current_num_processes = num_processes
+    list_of_processes.sort(key = lambda process : process.arrive) #Sort list by arrival time
+    print("The following processes are sorted by arrival time:")
+    for index, p in enumerate(list_of_processes):
+        print("Process ID: {}\nProcess Arrival Time: {}\nProcess Priority: {}\nProcess CPU_Time: {}\n".format(p.p_id, p.arrive, p.priority, p.cpu_time))
+
+
+
+    # while current_num_processes > 0:
+    #     ready_queue = []
+    #     normal_queue = []
+    #     temp = []
+    #     for i in range(len(list_of_processes)):
+    #         pass
+
+
 
 
 
@@ -45,10 +75,13 @@ def main():
         print("File path {} does not exist. Exiting...".format(file_path))
         sys.exit()
     # Pass file into read_file function
-    record, num_processes, list_of_processes = read_file(file_path)
+    # Return number of processes and list of the processes
+    num_processes, list_of_processes = read_file(file_path)
     print("The number of processes to be processed: {}".format(num_processes))
     for index, p in enumerate(list_of_processes):
         print("Process ID: {}\nProcess Arrival Time: {}\nProcess Priority: {}\nProcess CPU_Time: {}\n".format(p.p_id, p.arrive, p.priority, p.cpu_time))
+    
+    run = scheduling_process(num_processes, list_of_processes)
 
 main()
 
